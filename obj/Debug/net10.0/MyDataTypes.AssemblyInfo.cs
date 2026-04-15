a282c9d7ee6309b3b6d87d6fc0163818b1fd0395
Lostigen <jimmy.lostigen@outlook.com>
2026-04-15 09:19:03 +0200

    main

--- obj/Debug/net10.0/MyDataTypes.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyDataTypes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyDataTypes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9da07bbf0400782b77db03f2ffe78db928dc59f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94ff702c8c7015c60781a4cfb1c40f328fc21b61")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyDataTypes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyDataTypes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
